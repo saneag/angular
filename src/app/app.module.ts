c081209c574b616634954d147296c88c36f5e237
saneag <garstea2002@mail.ru>
2022-09-24 19:51:11 +0300

    deleted unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
-import { ContactRoutingModule } from './contact/contact-routing.module';
-import { AboutRoutingModule } from './about/about-routing.module';
-import { HomeRoutingModule } from './home/home-routing.module';
-import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
-import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -22,13 +16,7 @@ import { ContactModule } from './contact/contact.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ContactRoutingModule,
-    AboutRoutingModule,
-    HomeRoutingModule,
-    HomeModule,
-    AboutModule,
-    ContactModule,
-  ],
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
