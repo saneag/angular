051ab5aff7a12ef577c4b2b5ce032e681d4eeb6d
saneag <garstea2002@mail.ru>
2022-09-26 11:48:13 +0300

    deleted html template, styles and moved all to component.ts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,8 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  template: `
+      <main>
+        <p [className]="'progress_placeholder'">Contact page under construction...</p>
+      </main>
+  `,
+  styles: [`
+    main{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .progress_placeholder{
+        font-size: 2rem;
+        font-family: Comfortaa, sans-serif;
+        color: #fff;
+      }
+    }
+  `]
 })
 export class ContactComponent implements OnInit {
 
